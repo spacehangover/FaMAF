067ca77a2f905ad497fc13f00f6f371a0124a40f
spacehangover <51838046+spacehangover@users.noreply.github.com>
2024-05-28 17:53:35 -0300

    Finished Ejercicio 5

--- Algoritmos 1/Haskell/guia4.hs
+++ Algoritmos 1/Haskell/guia4.hs
@@ -57,6 +57,40 @@ propD (x:xs) | (cuadrado x && verde x) = True
              | (cuadrado x && verde x) == False = propD xs    
 
 propE :: [Figura] -> Bool
-propE [] = False
-propE (x:xs) | circulo x==True = azul x && tam x < 10 && propE xs
+propE [] = True
+propE (x:xs) | circulo x == True = azul x && tam x < 10 && propE xs
              | otherwise = propE xs
+
+propF :: [Figura] -> Bool
+propF [] = True
+propF (x:xs) | triangulo x == True = not (azul x) && propF xs
+             | otherwise = propF xs
+
+propG :: [Figura] -> Bool
+propG [] = True
+propG (x:xs)| circulo x == True = not (amarillo x || verde x) && propG xs
+            | otherwise = propG xs
+
+propH :: [Figura] -> Bool
+propH [] = False
+propH (x:xs)    | (cuadrado x && tam x < 5) = True
+                | otherwise = propH xs
+            
+circuloRojo :: [Figura] -> Bool
+circuloRojo [] = True
+circuloRojo (x:xs)  | circulo x==True = rojo x && circuloRojo xs
+                    | otherwise = circuloRojo xs
+
+cuadradoRojo :: [Figura] -> Bool
+cuadradoRojo [] = True
+cuadradoRojo (x:xs)  | cuadrado x==True = rojo x && cuadradoRojo xs
+                     | otherwise = cuadradoRojo xs
+
+propI :: [Figura] -> Bool
+propI [] = True
+propI (x:xs)    | circuloRojo [x] = cuadradoRojo xs
+                | otherwise = propI xs
+
+-- propI :: [Figira] -> Bool
+-- propI [] = False
+-- propI (x:xs) 
