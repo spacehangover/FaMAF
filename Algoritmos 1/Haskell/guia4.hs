4eefda9c6905dceface172e8e4a490e1e5cd31b3
spacehangover <agusmasco3@gmail.com>
2024-05-29 10:15:24 -0300

    29-5

--- Algoritmos 1/Haskell/guia4.hs
+++ Algoritmos 1/Haskell/guia4.hs
@@ -89,4 +89,5 @@ cuadradoRojo (x:xs)  | cuadrado x==True = rojo x && cuadradoRojo xs
 propI :: [Figura] -> Bool
 propI [] = True
 propI (x:xs)    | circuloRojo [x] = cuadradoRojo xs
-                | otherwise = propI xs  
+                | otherwise = propI xs
+
